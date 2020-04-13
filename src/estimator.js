b807a93e1fc68fddcc0f44fa5296afa25b1bc404
faith <faith@marascientific.org>
2020-04-13 12:10:11 +0300

    fix decimal points

--- src/estimator.js
+++ src/estimator.js
@@ -2,16 +2,16 @@ const impact = {};
 const severeImpact = {};
 
 const calculateFactor = (data) => {
-  // const daysToDouble = 3;
-  // const numberOfDaysInAweek = 7;
-  // const numberOfDaysInAmonth = 30;
+  const daysToDouble = 3;
+  const numberOfDaysInAweek = 7;
+  const numberOfDaysInAmonth = 30;
   if (data.periodType === 'days') {
-    return Math.trunc((data.timeToElapse) / 3);
+    return Math.trunc((data.timeToElapse) / daysToDouble);
   }
   if (data.periodType === 'weeks') {
-    return Math.trunc((data.timeToElapse * 7) / 3);
+    return Math.trunc((data.timeToElapse * numberOfDaysInAweek) / daysToDouble);
   }
-  return Math.trunc((data.timeToElapse * 30) / 3);
+  return Math.trunc((data.timeToElapse * numberOfDaysInAmonth) / daysToDouble);
 };
 
 
@@ -26,11 +26,11 @@ const getCurrentlyAffected = (data) => {
 };
 
 const getHospitalBedsByRequestedTime = (data) => {
-  const impactSevereCases = Math.trunc(0.15 * impact.infectionsByRequestedTime);
-  const severeImpactSevereCases = Math.trunc(0.15 * severeImpact.infectionsByRequestedTime);
+  const impactSevereCases = 0.15 * impact.infectionsByRequestedTime;
+  const severeImpactSevereCases = 0.15 * severeImpact.infectionsByRequestedTime;
   impact.severeCasesByRequestedTime = impactSevereCases;
   severeImpact.severeCasesByRequestedTime = severeImpactSevereCases;
-  const availableBeds = Math.trunc(0.35 * data.totalHospitalBeds);
+  const availableBeds = 0.35 * data.totalHospitalBeds;
   impact.hospitalBedsByRequestedTime = Math.trunc(availableBeds - impactSevereCases);
   severeImpact.hospitalBedsByRequestedTime = Math.trunc(availableBeds - severeImpactSevereCases);
 };
