209424265cfa4f62f2de9a1e37fdb45d113dee4b
faith <faith@marascientific.org>
2020-04-13 11:30:54 +0300

    change the decimal rounding method

--- src/estimator.js
+++ src/estimator.js
@@ -6,12 +6,12 @@ const calculateFactor = (data) => {
   const numberOfDaysInAweek = 7;
   const numberOfDaysInAmonth = 30;
   if (data.periodType === 'days') {
-    return Math.floor((data.timeToElapse) / daysToDouble);
+    return Math.trunc((data.timeToElapse) / daysToDouble);
   }
   if (data.periodType === 'weeks') {
-    return Math.floor((data.timeToElapse * numberOfDaysInAweek) / daysToDouble);
+    return Math.trunc(data.timeToElapse * numberOfDaysInAweek) / daysToDouble;
   }
-  return Math.floor((data.timeToElapse * numberOfDaysInAmonth) / daysToDouble);
+  return Math.trunc(data.timeToElapse * numberOfDaysInAmonth) / daysToDouble;
 };
 
 
@@ -22,17 +22,17 @@ const getCurrentlyAffected = (data) => {
   severeImpact.currentlyInfected = severeEffect;
   impact.infectionsByRequestedTime = impactEffect * (2 ** calculateFactor(data));
   severeImpact.infectionsByRequestedTime = severeEffect * (2 ** calculateFactor(data));
-  // return { impact, severeImpact };
+  return { impact, severeImpact };
 };
 
 const getHospitalBedsByRequestedTime = (data) => {
-  const impactSevereCases = Math.floor(0.15 * impact.infectionsByRequestedTime);
-  const severeImpactSevereCases = Math.floor(0.15 * severeImpact.infectionsByRequestedTime);
+  const impactSevereCases = Math.trunc(0.15 * impact.infectionsByRequestedTime);
+  const severeImpactSevereCases = Math.trunc(0.15 * severeImpact.infectionsByRequestedTime);
   impact.severeCasesByRequestedTime = impactSevereCases;
   severeImpact.severeCasesByRequestedTime = severeImpactSevereCases;
-  const availableBeds = Math.floor(0.35 * data.totalHospitalBeds);
-  impact.hospitalBedsByRequestedTime = Math.floor(availableBeds - impactSevereCases);
-  severeImpact.hospitalBedsByRequestedTime = Math.floor(availableBeds - severeImpactSevereCases);
+  const availableBeds = Math.trunc(0.35 * data.totalHospitalBeds);
+  impact.hospitalBedsByRequestedTime = availableBeds - impactSevereCases;
+  severeImpact.hospitalBedsByRequestedTime = availableBeds - severeImpactSevereCases;
 };
 
 const covid19ImpactEstimator = (data) => {
